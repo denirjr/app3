ed03a33ba111923d5688b9016dddf301e08f51b6
denirjr <denir-junior19@live.com>
2018-08-01 18:22:53 -0300

    firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     let config = {
-      apiKey: 'AIzaSyDT89bWfNtSjjVKxEAuxpZtY7jESjAx6to',
-      authDomain: 'jta-instagram-clone-b7a32.firebaseapp.com',
-      databaseURL: 'https://jta-instagram-clone-b7a32.firebaseio.com',
-      projectId: 'jta-instagram-clone-b7a32',
-      storageBucket: 'jta-instagram-clone-b7a32.appspot.com',
-      messagingSenderId: '884393633680'
+      // put your config here
     };
 
     firebase.initializeApp(config);
